8d047045bb76c146bc1de6038f3749d9c4c0ac69
wangzhen <zhen51.wang@tcl.com>
2023-08-08 17:28:48 +0800

    基础布局、排版

--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.jetpack.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetpack.compose.ui.theme.JetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Text(
+                            "基础布局、排版",
+                            modifier = Modifier.padding(10.dp),
+                            color = Color.Black,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                        MessageCard(
+                            Message(
+                                "Colleague",
+                                "Hey, take a look at Jetpack Compose, it's great!"
+                            )
+                        )
+                    }
                 }
             }
         }
@@ -30,17 +57,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(data: Message) {
+    Row(modifier = Modifier.padding(all = 10.dp)) {
+        Image(
+            painter = painterResource(id = R.mipmap.ic_launcher),
+            contentDescription = stringResource(
+                id = R.string.app_name
+            ),
+            modifier = Modifier.size(size = 80.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Text(
+                text = data.author, color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge,
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                tonalElevation = 1.dp
+            ) {
+                Text(
+                    text = data.body,
+                    modifier = Modifier.padding(10.dp),
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+data class Message(
+    val author: String,
+    val body: String,
+)
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "Night Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
-fun GreetingPreview() {
+fun MessageCardPreview() {
     JetpackcomposeTheme {
-        Greeting("Android")
+        Surface {
+            MessageCard(Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"))
+        }
     }
 }
