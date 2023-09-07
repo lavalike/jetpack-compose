469178ee643f2052da96ee6541929a89bae65a7b
wangzhen <zhen51.wang@tcl.com>
2023-09-07 14:37:42 +0800

    手势检测

--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -22,6 +24,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -77,6 +80,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -125,6 +129,7 @@ class MainActivity : ComponentActivity() {
                     Box(modifier = Modifier.padding(contentPadding)) {
                         val listState = rememberLazyListState()
                         LazyColumn(state = listState) {
+                            item { GestureDetectComposable() }
                             item { DynamicBlurComposable() }
                             item { ImageBlurComposable() }
                             item { ImageColorMatrixComposable() }
@@ -177,6 +182,97 @@ class MainActivity : ComponentActivity() {
 }
 
 @Preview(showBackground = true)
+@Composable
+fun GestureDetectComposable() {
+    Column {
+        BuildTitle(
+            TitleData(
+                title = "手势检测",
+                description = "Compose 提供了多种 API，可帮助您检测用户互动生成的手势。" +
+                        "\n其中一些级别较高，旨在覆盖最常用的手势。例如，clickable 修饰符可用于轻松检测点击，此外它还提供无障碍功能，并在点按时显示视觉指示（例如涟漪）。" +
+                        "\n还有一些不太常用的手势检测器，它们在较低级别提供更大的灵活性，例如 PointerInputScope.detectTapGestures 或 PointerInputScope.detectDragGestures，但不提供额外功能。"
+            )
+        )
+        var tapText by remember {
+            mutableStateOf("Detect Tap Gestures")
+        }
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colorScheme.surface,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .clip(RoundedCornerShape(5.dp))
+                .padding(10.dp)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onPress = {
+                            tapText = "You Pressed."
+                        },
+                        onDoubleTap = {
+                            tapText = "You Double Tapped."
+                        },
+                        onLongPress = {
+                            tapText = "You Long Pressed."
+                        },
+                        onTap = {
+                            tapText = "You Tapped."
+                        },
+                    )
+                },
+            shadowElevation = 1.dp,
+            tonalElevation = 1.dp,
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(tapText)
+            }
+        }
+
+        var dragText by remember {
+            mutableStateOf("Detect Drag Gestures")
+        }
+        Surface(
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colorScheme.surface,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .clip(RoundedCornerShape(5.dp))
+                .padding(10.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDrag = { change, dragAmount ->
+                            dragText = "PointerType: ${change.type}\nDragAmount: $dragAmount"
+                        },
+                        onDragStart = {
+                            dragText = "On Drag Start"
+                        },
+                        onDragCancel = {
+                            dragText = "On Drag Cancel"
+                        },
+                        onDragEnd = {
+                            dragText = "On Drag End"
+                        },
+                    )
+                },
+            shadowElevation = 1.dp,
+            tonalElevation = 1.dp,
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(dragText)
+            }
+        }
+    }
+}
+
 @Composable
 fun ImageBlurComposable() {
     Column {
